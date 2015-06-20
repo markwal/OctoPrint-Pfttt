48358d6e97d697141f4d0b2df48d66133a0bee45
Mark Walker <markwal@hotmail.com>
2015-06-20 12:48:26 -0700

    Skeletal IFTTT plugin that just spams all events

--- octoprint_pfttt/__init__.py
+++ octoprint_pfttt/__init__.py
@@ -1,23 +1,30 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin.
-#
-# Take a look at the documentation on what other plugin mixins are available.
+import requests
 
 import octoprint.plugin
 
-class PftttPlugin(octoprint.plugin.TemplatePlugin):
-	# TODO Implement me!
-	pass
+class PftttPlugin(octoprint.plugin.SettingsPlugin,
+		octoprint.plugin.TemplatePlugin,
+		octoprint.plugin.EventHandlerPlugin):
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Pfttt Plugin"
+	def get_settings_defaults(self):
+		return dict(maker_channel_key="")
+
+	def get_template_configs(self):
+		return [dict(type="settings", custom_bindings=False)]
+
+	def on_event(self, event_name, payload):
+		key = self._settings.get(['maker_channel_key']);
+		if key is None or key == '':
+			return
+		event_body = { 'value1' : payload }
+		r = requests.post("https://maker.ifttt.com/trigger/{event}/with/key/{key}".format(
+			event=event_name, key=key), data=event_body)
+		self._logger.info("IFTTT request returned %s" % r.text)
+
+__plugin_name__ = "Pfttt"
 
 def __plugin_load__():
 	global __plugin_implementation__
